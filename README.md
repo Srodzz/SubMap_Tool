f1fe3e1212a47202d1635badda4f49825e65ef27
Steven Rodriguez <steven15.rgp@gmail.com>
2025-11-26 20:02:24 -0600

    update subfinder

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # SubMap_Tool
 Tool that blend sublist3r with nmap and automatically scans subdomains
+
+Search subdomains with sublist3r and scan each subdomain with nmap
+Create a txt with subdomains and other txt with info about each subdomain
+
+# Requiremets
+ - Kali linux / Ubuntu / Debian
+ - Python 3.x
+ - Sublist3r
+ - Nmap
+ - Git
+
+# Installation and Use
+
+```bash
+git clone https://github.com/Srodzz/SubMap_Tool.git
+cd SubMap_Tool
+chmod +x SubMap.py
+./SubMap.py domain.com
+```
--- SubMap.py
+++ SubMap.py
@@ -3,34 +3,45 @@ import sys
 import subprocess
 import shutil
 
-# Tool that blend sublist3r with nmap and automatically scans subdomains
+# Tool that blend subfinder with nmap and automatically scans subdomains
 
 # check if user add a domain
 
 if len(sys.argv) < 2:
     print(f"Use: {sys.argv[0]} domain.com")
+    sys.exit(1)
 
 domain = sys.argv[1]
-subli_output = f"subs_{domain}.txt"
+subfinder_output = f"subs_{domain}.txt"
 
-def run_sublister():
+def run_subfinder():
     print(""" 
         =====================================
-        Enumerating subdomains with sublist3r
+        Enumerating subdomains with subfinder
         =====================================
         """)
     
     #Exceptions
-    if shutil.which("sublist3r") is None:
-        raise SystemExit("sublist3r not install")
+    if shutil.which("subfinder") is None:
+        raise SystemExit("subfinder not install")
         sys.exit(1)
     
     try:
-        subprocess.run(["sublist3r", "-d", domain, "-o", subli_output], check=True)
-        print("Done. Results in:", subli_output)
+        subprocess.run(["subfinder", "-d", domain, "-silent", "-o", subfinder_output], check=True)
+        print("Done. Results in:", subfinder_output)
     except subprocess.CalledProcessError as e:
         print("The command Failed with code:", e.returncode)
         print("Exit (stderr):", e.stderr)
+
+# def run_httx():
+#     print("""
+#         ================================
+#         Searching only active subdomains 
+#         ================================
+#     """)
+
+#     if shutil.which("httpx")
+
 
 def run_nmap():
     print("""
@@ -41,12 +52,12 @@ def run_nmap():
     nmap_output= f"nmap_report_{domain}.txt"
 
     try:
-        subprocess.run(["nmap", "-iL", subli_output, "-sCV", "-Pn", "-n", "oN", nmap_output], check=True)
+
         print("Done. Results in", nmap_output)
         print(f"""
             ==================================
             Finish!
-            Subdomain save in: {subli_output}
+            Subdomain save in: {subfinder_output}
             Nmap report in: {nmap_output}
             ==================================
             """)
@@ -54,5 +65,5 @@ def run_nmap():
         print("The command Failed with code:", e.returncode)
         print("Exit (stderr):", e.stderr)
 
-run_sublister()
+run_subfinder()
 run_nmap()
